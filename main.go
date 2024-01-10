1b48d0bc6521529f7735c10db80a2bf84bd73c05
Victor Toso <victortoso@redhat.com>
2024-01-10 17:01:33 +0100

    do testing benchmark too
    Signed-off-by: Victor Toso <victortoso@redhat.com>

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/xml"
 	"fmt"
 	"runtime"
+	"testing"
 
 	"kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/api"
 	"libvirt.org/go/libvirtxml"
@@ -13,23 +14,64 @@ import (
 //go:embed testdata/vmi-pvc-clipboard.xml
 var libvirtDomainXML []byte
 
-func main() {
+func BenchmarkLibvirtxml(b *testing.B) {
+	domain := new(libvirtxml.Domain)
+	if err := xml.Unmarshal(libvirtDomainXML, domain); err != nil {
+		panic(err)
+	}
+}
+
+func BenchmarkKubevirtSchema(b *testing.B) {
+	domain := new(api.DomainSpec)
+	if err := xml.Unmarshal(libvirtDomainXML, domain); err != nil {
+		panic(err)
+	}
+}
+
+func do_test_benchmark() {
+	res1 := testing.Benchmark(BenchmarkLibvirtxml)
+	fmt.Printf("Libvirtxml:\n")
+	fmt.Printf("Memory allocations  : %d \n", res1.MemAllocs)
+	fmt.Printf("Number of bytes ... : %d \n", res1.MemBytes)
+	fmt.Printf("Number of run ..... : %d \n", res1.N)
+	fmt.Printf("Time taken ........ : %s \n\n", res1.T)
+
+	res2 := testing.Benchmark(BenchmarkKubevirtSchema)
+	fmt.Printf("Kubevirt Schema:\n")
+	fmt.Printf("Memory allocations : %d \n", res2.MemAllocs)
+	fmt.Printf("Number of bytes .. : %d \n", res2.MemBytes)
+	fmt.Printf("Number of run .... : %d \n", res2.N)
+	fmt.Printf("Time taken ....... : %s \n\n", res2.T)
+
+	fmt.Printf("+ Libvirtxml > Schema by %d bytes\n\n", res1.MemBytes-res2.MemBytes)
+}
+
+func do_runtime_check() {
 	var m1, m2, m3 runtime.MemStats
-	var s1 api.DomainSpec
-	var s2 libvirtxml.Domain
+	var s1 libvirtxml.Domain
+	var s2 api.DomainSpec
 
 	runtime.ReadMemStats(&m1)
-
 	if err := xml.Unmarshal(libvirtDomainXML, &s1); err != nil {
 		panic(err)
 	}
 	runtime.ReadMemStats(&m2)
-
 	if err := xml.Unmarshal(libvirtDomainXML, &s2); err != nil {
 		panic(err)
 	}
 	runtime.ReadMemStats(&m3)
 
-	fmt.Printf("[Kubevirt  ] total=%d, mallocs=%d\n", m2.TotalAlloc-m1.TotalAlloc, m2.Mallocs-m1.Mallocs)
-	fmt.Printf("[Libvirtxml] total=%d, mallocs=%d\n", m3.TotalAlloc-m2.TotalAlloc, m3.Mallocs-m2.Mallocs)
+	total1 := m2.TotalAlloc - m1.TotalAlloc
+	total2 := m3.TotalAlloc - m2.TotalAlloc
+	fmt.Printf("[Libvirtxml] total=%d, mallocs=%d\n", total1, m2.Mallocs-m1.Mallocs)
+	fmt.Printf("[Kubevirt  ] total=%d, mallocs=%d\n\n", total2, m3.Mallocs-m2.Mallocs)
+	fmt.Printf("+ Libvirtxml > Schema by %d bytes\n\n", total1-total2)
+}
+
+func main() {
+	fmt.Println("* Using testing.Benchmark\n")
+	do_test_benchmark()
+
+	fmt.Println("* Using runtime.ReadMemStats\n")
+	do_runtime_check()
 }
